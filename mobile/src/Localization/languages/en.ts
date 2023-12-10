f615627ef3b6cbdf461acf981366deed12842c87
kawasaki <iduchungho@gmail.com>
2023-12-10 23:06:36 +0700

    refactor: rm src tree

--- mobile/src/Localization/languages/en.ts
+++
@@ -1,8 +0,0 @@
-import { LocalizationKey } from "../keys";
-
-export const en = {
-  [LocalizationKey.WELCOME]: "Welcome",
-  [LocalizationKey.HOME]: "Home",
-  [LocalizationKey.START]: "Start",
-  [LocalizationKey.LOADING]: "Loading",
-};
