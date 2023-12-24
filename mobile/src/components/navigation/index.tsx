b71d0a20ada272a4c31f5e1c5b4945fb97197918
Chinh Lâm Điền <lamdienchinhtd9a2@gmail.com>
2023-12-24 21:15:45 +0700

    fix: fix camera run in background

--- mobile/src/components/navigation/index.tsx
+++ mobile/src/components/navigation/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { StatusBar } from "react-native";
-import { createNativeStackNavigator} from "@react-navigation/native-stack";
-import { NavigationContainer} from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
 import { MainNavigator } from "./main";
 import { WelcomeContainer } from "@/screens/welcome";
 import { RootScreens } from "@/screens";
 import { OnboardingContainer } from "@/screens/onboarding";
 import { ScannerContainer } from "@/screens/scan/scanner-container";
-
 
 export type RootStackParamList = {
   [RootScreens.MAIN]: undefined;
@@ -19,14 +18,19 @@ export type RootStackParamList = {
   [RootScreens.PROFILE]: undefined;
 };
 
-const RootStack = createNativeStackNavigator<RootStackParamList>();
+const RootStack = createStackNavigator<RootStackParamList>();
 
-// @refresh reset
 const ApplicationNavigator = () => {
   return (
     <NavigationContainer>
       <StatusBar />
-      <RootStack.Navigator screenOptions={{ headerShown: false }}  initialRouteName={RootScreens.ONBOARDING}>
+      <RootStack.Navigator
+        detachInactiveScreens={true}
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName={RootScreens.ONBOARDING}
+      >
         <RootStack.Screen
           name={RootScreens.ONBOARDING}
           component={OnboardingContainer}
@@ -38,12 +42,10 @@ const ApplicationNavigator = () => {
         <RootStack.Screen
           name={RootScreens.MAIN}
           component={MainNavigator}
-          options={{}}
         />
         <RootStack.Screen
           name={RootScreens.SCANNER}
           component={ScannerContainer}
-          options={{}}
         />
       </RootStack.Navigator>
     </NavigationContainer>
