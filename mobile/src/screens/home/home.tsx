7492a89e36d1f0f0b7b68e8d25f1506f89e7a150
tringuyenle <102273673+tringuyenle@users.noreply.github.com>
2023-12-27 15:54:43 +0700

    feat: add general state for all room in homepage

--- mobile/src/screens/home/home.tsx
+++ mobile/src/screens/home/home.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { View, Text, StyleSheet, Dimensions, FlatList } from "react-native";
 import { Header } from "@/components/header";
 import {BarChart} from 'react-native-gifted-charts';
+import { Colors } from "@/theme/variables";
+import { autoBatchEnhancer } from "@reduxjs/toolkit";
 
 const screenWidth = Dimensions.get("screen").width;
 const screenHeight = Dimensions.get("screen").height;
@@ -136,72 +138,57 @@ export const Home = () => {
     },
   ];
 
+  const DATA = [
+    { id: '1', room: 'room 1', in: 10, out: 100, total: 100 },
+    { id: '2', room: 'room 2', in: 10, out: 20, total: 30 },
+    { id: '3', room: 'room 3', in: 10, out: 20, total: 30 },
+    // Add more items as needed
+  ];
+
+  type RoomProps = {room: string, in: number, out: number, total: number};
+
+  const Room = (Room: RoomProps) => (
+    <View style={styles.room}>
+      <Text style={styles.title}>{Room.room}</Text>
+      <View style={{ flexDirection: 'column' ,alignItems: 'center'}}>
+        <View style={styles.total_card}>
+          <Text style={{fontWeight: 'bold', color: '#306fbb'}}>Total: {Room.total}</Text>
+        </View >
+        <View style={styles.in_card}>
+          <Text style={{fontWeight: 'bold', color: '#47b063'}}>In: {Room.in}</Text>
+        </View>
+        <View style={styles.out_card}>
+          <Text style={{fontWeight: 'bold', color: '#ef5e5e'}}>Out: {Room.out}</Text>
+        </View>
+      </View>
+    </View>
+  );
+
   const renderTitle = () => {
     return(
-      <View
-        style={{
-          marginBottom: 20,
-          flex: 1,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}>
+      <View style={styles.barchart_title}>
         <View style={{flexDirection: 'row', alignItems: 'center'}}>
-          <Text
-            style={{
-              fontSize: 18,
-              height: 22,
-              fontWeight: 'bold',
-              color: '#000000',
-            }}>
+          <Text style={styles.title}>
             Daily in Room A
           </Text>
         </View>
-        <View style={{
-          flexDirection: 'row',
-          
-        }}>
+        <View style={{ flexDirection: 'row' }}>
           <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <View
-              style={{
-                height: 12,
-                width: 12,
-                borderRadius: 6,
-                backgroundColor: '#34A853',
-                marginRight: 8,
-              }}
-            />
-            <Text
-              style={{
-                width: 30,
-                height: 16,
-                color: '#000000',
-              }}>
+            <View style={styles.in_status}/>
+            <Text style={styles.text}>
               In
             </Text>
           </View>
           <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <View
-              style={{
-                height: 12,
-                width: 12,
-                borderRadius: 6,
-                backgroundColor: '#ED4A4A',
-                marginRight: 8,
-              }}
-            />
-            <Text
-              style={{
-                width: 30,
-                height: 16,
-                color: '#000000',
-              }}>
+            <View style={styles.out_status}/>
+            <Text style={styles.text}>
               Out
             </Text>
           </View>
         </View>
       </View>
     )
-}
+  }
 
   return (
     <View style={styles.container}>
@@ -209,19 +196,7 @@ export const Home = () => {
         <Header title="Home" />
       </View>
       <View style={styles.content_container}>
-        <View
-          style={{
-            backgroundColor: 'white',
-            padding: 20,
-            paddingTop: 30,
-            borderRadius: 10,
-            shadowOffset: {
-              width: 0,
-              height: 0,
-            },
-            shadowOpacity: 0.2,
-            shadowRadius: 5,
-          }}>
+        <View style={styles.component_content}>
           {renderTitle()}
           <BarChart
             data={barData}
@@ -238,6 +213,14 @@ export const Home = () => {
             maxValue={75}
           />
         </View>
+        <View style={styles.component_content}>
+          <FlatList
+            data={DATA}
+            horizontal={true}
+            renderItem={({item}) => <Room room={item.room} in={item.in} out={item.out} total={item.total} />}
+            keyExtractor={item => item.id}
+          />
+        </View>
       </View>
     </View>
   );
@@ -246,13 +229,97 @@ export const Home = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFFFFF",
+    backgroundColor: '#f4f7fb',
   },
   header_container: {
-
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+  },
+  barchart_title: {
+    marginBottom: 20,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  in_status: {
+    height: 12,
+    width: 12,
+    borderRadius: 6,
+    backgroundColor: '#34A853',
+    marginRight: 8,
+  },
+  out_status: {
+    height: 12,
+    width: 12,
+    borderRadius: 6,
+    backgroundColor: '#ED4A4A',
+    marginRight: 8,
+  },
+  text: {
+    width: 30,
+    height: 16,
+    color: '#000000',
   },
   content_container: {
     paddingTop: 30,
     paddingHorizontal: 0.05 * screenWidth,
-  }
+  },
+  component_content: {
+    backgroundColor: 'white',
+    padding: 20,
+    paddingTop: 30,
+    borderRadius: 10,
+    marginBottom: 20,
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+  },
+  room: {
+    backgroundColor: Colors.WHITE,
+    alignItems: 'center',
+    padding: 10,
+    width: 100,
+    marginRight: 5,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#7c7c7c',
+  },
+  total_card: {
+    backgroundColor: '#d9e4f3', 
+    paddingTop: 10, 
+    paddingBottom: 10, 
+    alignItems: 'center',
+    width: 80,
+    borderTopStartRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  in_card: {
+    width: 80,
+    backgroundColor: '#e0f2e5', 
+    paddingTop: 10, 
+    paddingBottom: 10, 
+    alignItems: 'center', 
+  },
+  out_card: {
+    backgroundColor: '#fde4e4', 
+    paddingTop: 10, 
+    paddingBottom: 10, 
+    alignItems: 'center', 
+    width: 80,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  title: {
+    fontSize: 18,
+    height: 22,
+    fontWeight: 'bold',
+    color: '#000000',
+  },
 });
