11a1fd153bdc3cf3a7f132da2b4e4095eabb0c42
kawasaki <iduchungho@gmail.com>
2023-12-29 16:53:43 +0700

    chore: fix many connection

--- server/pkg/database/database.go
+++ server/pkg/database/database.go
@@ -2,12 +2,27 @@ package database
 
 import (
 	"scsystem/pkg/utils"
+	"sync"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+var connection *gorm.DB = nil
+var lock *sync.Mutex = &sync.Mutex{}
+
 func Connection() (*gorm.DB, error) {
-	dsn, _ := utils.ConnectionURLBuilder("mysql")
-	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if connection == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		if connection == nil {
+			dsn, _ := utils.ConnectionURLBuilder("mysql")
+			conn, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+			if err != nil {
+				return nil, err
+			}
+			connection = conn
+		}
+	}
+	return connection, nil
 }
